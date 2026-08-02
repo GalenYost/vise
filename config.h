eefc6610d76b33d37c0089a5d4f6850fec876e64
GalenYost <brawlstarser086@gmail.com>
2026-08-02 17:50:22 +0300

    config: set default ENABLE_LOG to 1

--- config.h
+++ config.h
@@ -4,5 +4,5 @@
 #define COMPILER_NAME "Vise"      // just a visual display name
 
 // LOGGING
-#define ENABLE_LOGS 0             // 1 - enable, 0 - disable
+#define ENABLE_LOGS 1             // 1 - enable, 0 - disable
 #define LOG_FILE_PATH "build.log" // generated next to the compiler executable, meaning the path is relative
