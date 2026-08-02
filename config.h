e0b2265d38a3009ebb10d797331010b4b8caaf08
GalenYost <brawlstarser086@gmail.com>
2026-08-02 16:50:03 +0300

    utils: use config

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
-#define BUILD_FOLDER "build"
-#define EXEC_NAME "vise"
-#define COMPILER_NAME "Vise"
+// GENERAL
+#define BUILD_FOLDER "build"      // relative build directory, to switch to absolute path use '/' prefix
+#define EXEC_NAME "vise"          // executable name
+#define COMPILER_NAME "Vise"      // just a visual display name
 
-#define LOG_FILE_PATH "build.log" 
+// LOGGING
+#define ENABLE_LOGS 0             // 1 - enable, 0 - disable
+#define LOG_FILE_PATH "build.log" // generated next to the compiler executable, meaning the path is relative
--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,8 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+#include "../config.h"
+
 typedef struct {
     char *data;
     size_t len;
@@ -28,16 +30,18 @@ typedef enum {
     FATAL,
 } LogLevel;
 
-#define DEFAULT_LOG_PATH "build.log"
-
 void log_to_console(LogLevel level, const char *fmt, ...);
 void log_to_file(LogLevel level, const char *path, const char *fmt, ...);
 
+#if ENABLE_LOGS
 #define LOG(level, ...) \
     do { \
         log_to_console(level, __VA_ARGS__); \
-        log_to_file(level, DEFAULT_LOG_PATH, __VA_ARGS__); \
+        log_to_file(level, LOG_FILE_PATH, __VA_ARGS__); \
     } while (0)
+#else
+#define LOG(level, ...) ((void)0)
+#endif
 
 void print_token(Token t);
 void print_node(Node *n);
